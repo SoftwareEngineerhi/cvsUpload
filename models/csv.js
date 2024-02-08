cdf6ed53ee716d491915e2c013da31a469c1c291
Nalini Rajput <138565991+SoftwareEngineerhi@users.noreply.github.com>
2024-02-08 15:34:40 +0530

    Update csv.js

--- models/csv.js
+++ models/csv.js
@@ -1,5 +1,7 @@
 /** ------------------ IMPORTING MONGOOSE ------------------ **/
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
 
 const fileSchema = new mongoose.Schema({
   fileName: {
@@ -17,7 +19,19 @@ const fileSchema = new mongoose.Schema({
   }
 });
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname,'..', CSV_PATH));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+});
 
+//static functions
+csvSchema.statics.uploadedCSV = multer({storage: storage, limits:{fileSize: 1 * 1024 * 1024}}).single('csv');
+csvSchema.statics.csvPath = CSV_PATH;
 
 
 /** ------------------ MAKING MODEL ------------------ **/
